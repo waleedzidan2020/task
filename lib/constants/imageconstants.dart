bb5b3a3a9ce4ce3d1dfedec522ef79e42b357c0b
waleedzidan2020 <83763887+waleedzidan2020@users.noreply.github.com>
2023-04-14 00:02:17 +0200

    add slider and category sections

--- lib/constants/imageconstants.dart
+++ lib/constants/imageconstants.dart
@@ -1,6 +1,10 @@
+import '../utilize/AppLocalization.dart';
+
 class ImageConstants{
 
  static String locationIcon ="assets/icons/Location_Icon.jpg" ;
  static String searchIcon ="assets/icons/Search_Icon.jpg" ;
+ static List<String> SliderImages=["assets/Images/SliderImages/1.jpg","assets/Images/SliderImages/1.jpg","assets/Images/SliderImages/1.jpg"];
+ static List<String> categoryImages=["assets/Images/CategoryImages/quick_order.jpg","assets/Images/CategoryImages/resturants.jpg","assets/Images/CategoryImages/Grocery.jpg"];
 
 }
--- lib/screens/MainScreen.dart
+++ lib/screens/MainScreen.dart
@@ -1,32 +1,53 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:some_design/widgets/SearchSection.dart';
 import '../utilize/AppLocalization.dart';
 import '../widgets/AppBarSection.dart';
+import '../widgets/CategorySection.dart';
+import '../widgets/ReOrderSection.dart';
+import '../widgets/SliderSection.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key,required this.Func}) : super(key: key);
+  const HomeScreen({Key? key, required this.Func}) : super(key: key);
 
   final VoidCallback Func;
+
   @override
   Widget build(BuildContext context) {
-    return  Directionality(
-      textDirection:AppLocatization.of(context).locale.languageCode=="ar"?TextDirection.rtl:TextDirection.ltr ,
+    return Directionality(
+      textDirection: AppLocatization.of(context).locale.languageCode == "ar"
+          ? TextDirection.rtl
+          : TextDirection.ltr,
       child: Scaffold(
         backgroundColor: Colors.white,
         body: SafeArea(
           child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
             child: CustomScrollView(
               slivers: [
                 SliverToBoxAdapter(
                   child: Column(
-                    children:  [
-                      AppBarSection(func: () => Func(),),
-                     const SizedBox(
+                    children: [
+                      AppBarSection(
+                        func: () => Func(),
+                      ),
+                      const SizedBox(
                         height: 16,
                       ),
-                   const   SearchSection()
+                      const SearchSection(),
+                      const SizedBox(
+                        height: 21,
+                      ),
+                      const SliderSection(),
+                      const SizedBox(
+                        height: 24,
+                      ),
+                    const  CategorySection(),
+                      const SizedBox(
+                        height: 25,
+                      ),
+                    const  ReOrderSection()
                     ],
                   ),
                 ),
